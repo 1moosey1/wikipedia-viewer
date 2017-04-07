b401f89f958e7496b9bafc5f651df63b8e36336a
Juan Gonzalez <jgonzalez498@gmail.com>
2017-04-06 23:22:39 -0700

    Minor fixes and improvements

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function randomArticle() {
 
 function searchArticles() {
   
-  var url = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=extracts&exsentences=2&exlimit=10&exintro&explaintext&gsrsearch=";
+  var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=extracts&exsentences=2&exlimit=10&exintro&explaintext&gsrsearch=";
   var search = $("#search").val() + "&callback=?";
   
   $.getJSON(url + search, displayArticles);
@@ -28,19 +28,19 @@ function displayArticles(data) {
   if(data.hasOwnProperty("query")) {
     
     var articles = data.query.pages;
-    var articleHTML = $("<div class='row'><div class='col-lg-8 col-lg-offset-2 listitem' id='click'></div></div>");
+    var articleHTML = 
+      $("<div class='col-md-8 col-md-offset-2 col-xs-8 col-xs-offset-2 listitem'></div>");
   
     Object.keys(articles).forEach(function(key) {
     
-      var html = articleHTML.clone();
-      var listItem = html.find(".listitem"); 
+      var listItem = articleHTML.clone();
       listItem.click(function() {
-        window.open("http://en.wikipedia.org/?curid=" + articles[key].pageid);
+        window.open("https://en.wikipedia.org/?curid=" + articles[key].pageid);
       });
-    
+
       listItem.append("<h3>" + articles[key].title + "</h3>");
       listItem.append("<p>" + articles[key].extract + "</p>");
-      $("#insert").append(html);
+      $("#insert").append(listItem);
     });
   }
   else
